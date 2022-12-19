18941f733e82901b422be8c427ef78ce3b5e854a
Chris Rowles <c.rowles@sentrybay.com>
2022-12-19 21:21:36 +0000

    all

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -30,73 +30,79 @@ onMounted(() => {
   <main>
     <div class="container-fluid px-5 py-4">
       <Header decor-title="Raspbian Monitor" title="Dashboard"></Header>
-      <div class="row mt-3">
-        <div class="col d-flex align-items-stretch mt-3 mt-md-0">
-          <StatCard title="Platform" bg="dark" icon="fa-solid fa-server">
-            <template #detail>
-              <PlatformDetail v-if="system" :detail="system.platform" />
-            </template>
-          </StatCard>
+      <section class="page-section mt-0">
+        <div class="row mt-3">
+          <div class="col d-flex align-items-stretch mt-3 mt-md-0">
+            <StatCard title="Platform" bg="dark" icon="fa-solid fa-server">
+              <template #detail>
+                <PlatformDetail v-if="system" :detail="system.platform" />
+              </template>
+            </StatCard>
+          </div>
+          <div class="col d-flex align-items-stretch mt-3 mt-md-0">
+            <StatCard title="CPU" bg="success" icon="fa-solid fa-tachometer-alt">
+              <template #detail>
+                <CpuDetail v-if="system" :detail="system.cpu" />
+              </template>
+            </StatCard>
+          </div>
+          <div class="col d-flex align-items-stretch mt-3 mt-md-0">
+            <StatCard title="Memory" bg="success" icon="fa-solid fa-server">
+              <template #detail>
+                <UsageDetail v-if="system" :detail="system.mem" />
+              </template>
+            </StatCard>
+          </div>
+          <div class="col d-flex align-items-stretch mt-3 mt-md-0">
+            <StatCard title="Disk" bg="dark" icon="fa-solid fa-hard-drive">
+              <template #detail>
+                <UsageDetail v-if="system" :detail="system.disk" />
+              </template>
+            </StatCard>
+          </div>
         </div>
-        <div class="col d-flex align-items-stretch mt-3 mt-md-0">
-          <StatCard title="CPU" bg="success" icon="fa-solid fa-tachometer-alt">
-            <template #detail>
-              <CpuDetail v-if="system" :detail="system.cpu" />
-            </template>
-          </StatCard>
-        </div>
-        <div class="col d-flex align-items-stretch mt-3 mt-md-0">
-          <StatCard title="Memory" bg="success" icon="fa-solid fa-server">
-            <template #detail>
-              <UsageDetail v-if="system" :detail="system.mem" />
-            </template>
-          </StatCard>
-        </div>
-        <div class="col d-flex align-items-stretch mt-3 mt-md-0">
-          <StatCard title="Disk" bg="dark" icon="fa-solid fa-hard-drive">
-            <template #detail>
-              <UsageDetail v-if="system" :detail="system.disk" />
-            </template>
-          </StatCard>
-        </div>
-      </div>
-      <div class="row mt-3">
-        <div class="col">
-          <div class="card">
-            <div class="card-header bg-transparent border-0 d-flex justify-content-center py-4">
-              <h2 class="header">Usage Overview</h2>
-            </div>
-            <div class="card-body">
-              <div v-if="system" class="row">
-                <div class="col mt-3 mt-md-0">
-                  <DonutChart :data="system.cpu.temp" label="Temp" />
-                </div>
-                <div class="col mt-3 mt-md-0">
-                  <DonutChart :data="system.cpu.usage" label="CPU" />
-                </div>
-                <div class="col mt-3 mt-md-0">
-                  <DonutChart :data="system.mem.percent" label="Memory" />
-                </div>
-                <div class="col mt-3 mt-md-0">
-                  <DonutChart :data="system.disk.percent" label="Disk" />
+      </section>
+      <section class="page-section">
+        <div class="row">
+          <div class="col">
+            <div class="card border-0 shadow-lg">
+              <div class="card-header bg-transparent border-0 d-flex justify-content-center py-4">
+                <h2 class="header">Usage Overview</h2>
+              </div>
+              <div class="card-body">
+                <div v-if="system" class="row">
+                  <div class="col mt-3 mt-md-0">
+                    <DonutChart :data="system.cpu.temp" label="Temp" />
+                  </div>
+                  <div class="col mt-3 mt-md-0">
+                    <DonutChart :data="system.cpu.usage" label="CPU" />
+                  </div>
+                  <div class="col mt-3 mt-md-0">
+                    <DonutChart :data="system.mem.percent" label="Memory" />
+                  </div>
+                  <div class="col mt-3 mt-md-0">
+                    <DonutChart :data="system.disk.percent" label="Disk" />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div class="row mt-3">
-        <div class="col">
-          <div class="card">
-            <div class="card-header bg-transparent border-0 d-flex justify-content-center py-4">
-              <h2 class="header">Top Processes</h2>
-            </div>
-            <div class="card-body">
-              <DataTable v-if="system" type="horizontal" :data="system.processes" />
+      </section>
+      <section class="page-section">
+        <div class="row">
+          <div class="col">
+            <div class="card border-0 shadow-lg">
+              <div class="card-header bg-transparent border-0 d-flex justify-content-center py-4">
+                <h2 class="header">Top Processes</h2>
+              </div>
+              <div class="card-body">
+                <DataTable v-if="system" type="horizontal" :data="system.processes" />
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      </section>
     </div>
   </main>
-</template>
+</template> 
